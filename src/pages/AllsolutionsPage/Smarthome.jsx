88cbc96707b4b5a97c87e1569ebe6a6fe3e3b532
HariprasathIT <hariprasath@adhiba.com>
2025-12-25 12:30:58 +0530

    solution 1 res

--- src/pages/AllsolutionsPage/Smarthome.jsx
+++ src/pages/AllsolutionsPage/Smarthome.jsx
@@ -146,11 +146,11 @@ function Sectiontwo() {
             <section className="max-w-[1540px] mx-auto px-4">
                 <div>
                     {/* Header */}
-                    <div className="text-center py-16">
-                        <h2 className="text-2xl md:text-[28px] lg:text-[30px] font-bold text-red-600 mb-2">
+                    <div className="text-center sm:py-16 py-8 md:py-12 lg:py-16">
+                        <h2 className="text-[18px] md:text-[28px] lg:text-[30px] font-bold text-red-600 mb-2">
                             NYSTAI'S HOME AUTOMATION
                         </h2>
-                        <p className="text-sm md:text-[15px] text-gray-600 tracking-wider">
+                        <p className="text-[14px] md:text-[15px] text-gray-600 tracking-wider">
                             The subtext helps customers quickly understand the value or specific
                         </p>
                     </div>
@@ -203,7 +203,7 @@ function Sectiontwo() {
                                         className="w-14 h-14 object-contain"
                                     />
                                 </div>
-                                <h3 className="text-[#dc3545] font-bold text-xl mb-3 tracking-wide">
+                                <h3 className="text-[#dc3545] font-bold sm:text-xl text-[16px] mb-3 tracking-wide">
                                     {feature.title}
                                 </h3>
                                 <p className="text-gray-600 text-sm leading-relaxed">
@@ -249,10 +249,10 @@ function Sectionthree() {
         <section className="max-w-[1540px] mx-auto px-4">
             {/* Header */}
             <div className="text-center py-8 md:py-12 lg:py-16">
-                <h2 className="text-2xl md:text-[28px] lg:text-[30px] font-bold text-red-600 mb-2">
+                <h2 className="text-[18px] md:text-[28px] lg:text-[30px] font-bold text-red-600 mb-2">
                     WHAT WE OFFER
                 </h2>
-                <p className="text-sm md:text-[15px] text-gray-600 tracking-wider px-4">
+                <p className="text-[14px] md:text-[15px] text-gray-600 tracking-wider px-4">
                     The subtext helps customers quickly understand the value or specific
                 </p>
             </div>
@@ -303,7 +303,7 @@ function Sectionthree() {
                             }`}
                         />
                         <div className="absolute inset-0 p-6 flex flex-col justify-end text-white">
-                            <h3 className="text-xl font-bold mb-2">
+                            <h3 className="sm:text-xl text-[18px] font-bold mb-2">
                                 {card.title}
                             </h3>
                             <p className={`text-sm transition-all duration-300 ${
@@ -351,11 +351,11 @@ function SectionFour() {
         <>
             <section className="max-w-[1540px] mx-auto px-4">
                 {/* Header */}
-                <div className="text-center py-16">
-                    <h2 className="text-2xl md:text-[28px] lg:text-[30px] font-bold text-red-600 mb-2">
+                <div className="text-center sm:py-16 py-8 md:py-12 lg:py-16">
+                    <h2 className="text-[18px] md:text-[28px] lg:text-[30px] font-bold text-red-600 mb-2">
                         FEATURES
                     </h2>
-                    <p className="text-sm md:text-[15px] text-gray-600 tracking-wider">
+                    <p className="text-[14px] md:text-[15px] text-gray-600 tracking-wider">
                         How the Nystai Smart Home System Works
                     </p>
                 </div>
@@ -367,10 +367,10 @@ function SectionFour() {
                             <div className="w-15 h-15 flex items-center justify-center mb-6">
                                 <img src={feature.icon} alt="feature icon" className="w-full h-[72px] object-contain" />
                             </div>
-                            <h3 className="text-[20px] font-semibold mb-3">
+                            <h3 className=" sm:text-xl text-[16px]  font-semibold mb-3">
                                 {feature.title}
                             </h3>
-                            <p className="text-[15px] text-gray-600">
+                            <p className="text-sm text-gray-600">
                                 {feature.description}
                             </p>
                         </div>
